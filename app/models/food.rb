6ee0db6af81880c5ca348966f65d7ad7bb6972c5
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-06-10 11:23:34 -0700

    heroku setup pt3

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,10 @@
 class Food
-    DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'flavorbase_development'})
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'flavorbase_development'})
+    end
 
     def self.all
         results = DB.exec(
